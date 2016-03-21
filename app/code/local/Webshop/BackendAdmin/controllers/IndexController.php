6d0cc2492073a7e0cb7e32c08a9bd48390e80838
Yanick Schraner <yanick.schraner@gmail.com>
2016-03-21 13:53:37 +0100

    updated IndexController of BackendAdmin

--- app/code/local/Webshop/BackendAdmin/controllers/IndexController.php
+++ app/code/local/Webshop/BackendAdmin/controllers/IndexController.php
@@ -1,9 +1,33 @@
 <?php
+require_once "Mage/Adminhtml/controllers/IndexController.php";
 class Webshop_BackendAdmin_IndexController extends Mage_Adminhtml_Controller_Action
 {
+    /**
+     * Render specified template
+     *
+     * @param string $tplName
+     * @param array $data parameters required by template
+     */
+    protected function _outTemplate($tplName, $data = array())
+    {
+        $this->_initLayoutMessages('adminhtml/session');
+        $block = $this->getLayout()->createBlock('adminhtml/template')->setTemplate("$tplName.phtml");
+        foreach ($data as $index => $value) {
+            $block->assign($index, $value);
+        }
+        $html = $block->toHtml();
+        Mage::getSingleton('core/translate_inline')->processResponseBody($html);
+        $this->getResponse()->setBody($html);
+    }
+    
     public function indexAction()
     {
-        $this->loadLayout();
-        $this->renderLayout();
+        $session = Mage::getSingleton('admin/session');
+        $url = $session->getUser()->getStartupPageUrl();
+        if ($session->isFirstPageAfterLogin()) {
+            // retain the "first page after login" value in session (before redirect)
+            $session->setIsFirstPageAfterLogin(true);
+        }
+        $this->_redirect("backendadmin");
     }
 }
