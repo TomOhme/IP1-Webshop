0224b8a07bc59ec04a02fb403c4dc5478e8c0a2b
Janis Angst <janis.angst@students.fhnw.ch>
2016-03-23 13:21:35 +0100

    changed header and navigation

---
+++ administration/include/footer.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Janis
+ * Date: 16.03.2016
+ * Time: 14:14
+ */
+?>
+
+<div class="left">
+    <ul>
+        <li><a href="">Produkte</a></li>
+        <li><a href="">Über Uns</a></li>
+        <li><a href="">Warenkorb</a></li>
+        <li><a href="">Registreiren</a></li>
+        <li><a href="">Admin Login</a></li>
+    </ul>
+</div>
+<div class="right">
+
+</div>
