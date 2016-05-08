e994fbbb0d186af8f707d87e9bba7da542a9383d
Yanick Schraner <yanick.schraner@gmail.com>
2016-05-08 16:44:58 +0200

    added "5 Rappen Rundung" and discount module

--- app/code/local/Webshop/BackendRabattsystem/Model/Observer.php
+++ app/code/local/Webshop/BackendRabattsystem/Model/Observer.php
@@ -7,56 +7,54 @@
  * Sets the discount to the displayed product if discount criteria fulfilled
  */
 
-class Webshop_BackendRabattsystem_Model_Observer
-{
+class Webshop_BackendRabattsystem_Model_Observer{
 
-    public function get_final_price($observer)
-    {
+    public function get_final_price($observer){
         /* Check if the customer is logged in, if so, then we proceed */
-        if(Mage::getSingleton('customer/session')->isLoggedIn())
-        {
-            //set up needed variables
-            $customer_id = Mage::getSingleton('customer/session')->getCustomerId();
-            $fromDate = date('Y-m-d H:i:s', strtotime(date('Y-01-01')));
-            $toDate = date('Y-m-d H:i:s');
-            $totalOrderAmount = 0;
-            $discount = 0;
+        if(Mage::getSingleton('customer/session')->isLoggedIn()){
             $event = $observer->getEvent();
             $product = $event->getProduct();
             $product->original_price = $product->getPrice();
-            $productID = $product->getId();
+            //Check if product is allready discounted
+            if($product->getSpecialPrice() == null){
+                //Setup needed values
+                $customer_id = Mage::getSingleton('customer/session')->getCustomerId();
+                $fromDate = date('Y-m-d H:i:s', strtotime(date('Y-01-01')));
+                $toDate = date('Y-m-d H:i:s');
+                $totalOrderAmount = 0;
+                $discount = 0;
 
-            //receive orders of loged in user containing displayed product
-            $orders = Mage::getResourceModel('sales/order_item_collection')
-                ->addFieldToSelect('*')
-                ->addAttributeToFilter('product_id', array('eq' => $productID))
-            ;
-            $orders->getSelect()->join(array('sales_order' => Mage::getSingleton('core/resource')->getTableName('sales/order')), 'main_table.order_id = sales_order.entity_id and customer_id=' . $customer_id, array('sales_order.entity_id'));
-            $orders = $orders->getData();
-            foreach($orders as $order){
-                if($fromDate <= $order['created_at'] && $order['created_at'] <= $toDate){
-                    $totalOrderAmount += $order['base_row_total_incl_tax'];
+                //receive orders of loged in user containing displayed product
+                $orders = Mage::getResourceModel('sales/order_collection')
+                    ->addFieldToSelect('base_grand_total')
+                    ->addFieldToFilter('customer_id', $customer_id)
+                    ->addAttributeToFilter('created_at', array('from' => $fromDate, 'to' => $toDate))
+                ;
+                $orders = $orders->getData();
+                foreach ($orders as $order){
+                    $totalOrderAmount += $order['base_grand_total'];
                 }
-            }
+                $totalOrderAmount += Mage::helper('checkout')->formatPrice(Mage::getSingleton('checkout/cart')->getQuote()->getGrandTotal());
 
-            //Get discount data from the database
-            $resource = Mage::getSingleton('core/resource');
-            $readConnection = $resource->getConnection('core_read');
-            $query = 'SELECT * FROM custom_discount WHERE productID = ' . $productID .';';
-            $results = $readConnection->fetchAll($query);
+                //Get discount data from the database
+                $resource = Mage::getSingleton('core/resource');
+                $readConnection = $resource->getConnection('core_read');
+                $query = 'SELECT * FROM custom_discount;';
+                $results = $readConnection->fetchAll($query);
 
-            //Check if the discount criteria are fulfilled
-            foreach ($results as $result){
-                if($discount < $result['discount'] && $totalOrderAmount > $result['setAfter']){
-                    $discount = $result['discount'];
+                //Check if the discount criteria are fulfilled
+                foreach ($results as $result){
+                    if($discount < $result['discount'] && $totalOrderAmount > $result['setAfter']){
+                        $discount = $result['discount'];
+                    }
                 }
+
+                //Update Price as discount price
+                $final_price = $product->original_price * (1 - $discount);
+                $product->setFinalPrice($final_price);
+
+                return $this;
             }
-
-            //Update Price as discount price
-            $final_price = $product->original_price * (1 - $discount);
-            $product->setFinalPrice($final_price);
-
-            return $this;
         }
     }
 }
