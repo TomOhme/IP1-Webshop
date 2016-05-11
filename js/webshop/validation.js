21f616a2de2c7930ec01b8a2f9b958eb9e5592dd
martinfrancois <fracoisma@hotmail.com>
2016-05-11 08:56:34 +0200

    validation for registration fields

--- js/webshop/validation.js
+++ js/webshop/validation.js
@@ -3,6 +3,9 @@ François 20160510 - Validating input fields
  */
 
 // Validation of PLZ, must be exactly 4 numerals
-Validation.add('validate-plz', 'Bitte geben Sie eine 4-stellige PLZ ein.', function(v) {
+Validation.add('validate-ch-plz', 'Bitte geben Sie eine 4-stellige PLZ ein.', function(v) {
     return Validation.get('IsEmpty').test(v) || /^\d{4}$/.test(v);
+});
+Validation.add('validate-ch-phone', 'Bitte geben Sie eine korrekte Telefonnummer ein.', function(v) {
+    return Validation.get('IsEmpty').test(v) || /^\d{10}$/.test(v);
 });
