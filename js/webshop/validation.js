e55da80c1f24a4d67aaf93e7c1becb9bc3d74e71
martinfrancois <fracoisma@hotmail.com>
2016-05-22 23:44:23 +0200

    validation of password

--- js/webshop/validation.js
+++ js/webshop/validation.js
@@ -9,3 +9,7 @@ Validation.add('validate-ch-plz', 'Bitte geben Sie eine 4-stellige PLZ ein.', fu
 Validation.add('validate-ch-phone', 'Bitte geben Sie eine korrekte Telefonnummer ein.', function(v) {
     return Validation.get('IsEmpty').test(v) || /^\d{10}$/.test(v);
 });
+Validation.add('validate-max-password', 'Bitte geben Sie ein Passwort mit weniger als 840 Zeichen ein.', function(v) {
+    var pass=v
+    return !(pass.length>840);
+});
